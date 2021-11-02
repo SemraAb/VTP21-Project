a2c6c6a8c2a9e9b9f6c28bc12cac1c2040ef66b2
Samra Abdurahmanli <samra.abdurahmanli.a@asoiu.edu.az>
2021-11-02 23:13:41 +0400

    Change

--- dash/js/chart-js-config.js
+++ dash/js/chart-js-config.js
@@ -16,3 +16,26 @@ window.chartColors = {
     superwarning: '#fd7e14',
     danger: '#FC413F',
 };
+setTimeout(function start (){
+  
+    $('.bar').each(function(i){  
+      var $bar = $(this);
+      $(this).append('<span class="count"></span>')
+      setTimeout(function(){
+        $bar.css('width', $bar.attr('data-percent'));      
+      }, i*100);
+    });
+   
+  $('.count').each(function () {
+      $(this).prop('Counter',0).animate({
+          Counter: $(this).parent('.bar').attr('data-percent')
+      }, {
+          duration: 2000,
+          easing: 'swing',
+          step: function (now) {
+              $(this).text(Math.ceil(now) +'%');
+          }
+      });
+  });
+   
+  }, 500)
